2aac198ac05e60e7db1e8ced1455a66848dd4b3e
SpaceUK <71958442+SpaceUK3@users.noreply.github.com>
2024-10-08 16:43:16 +0300

    Homework
    Дзшчка )

---
+++ Course/2nd year/Lesson2/DZ/first/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace sfg
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int summ = num1 + num2;
+            System.Console.WriteLine(summ);
+        }
+    }
+}
---
+++ Course/2nd year/Lesson2/DZ/second/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace sfg
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int random = rnd.Next(1, 15);
+            System.Console.WriteLine(random);
+            System.Console.WriteLine("Я загадал число от 1 до 15, попробуй его угадать :)");
+            int NumOfUser = Convert.ToInt32(Console.ReadLine());
+            while (NumOfUser != random) 
+            {
+                if (NumOfUser > random)
+                {
+                    System.Console.WriteLine("Возьми меньше)");
+                    NumOfUser = Convert.ToInt32(Console.ReadLine());
+                }
+                else{
+                    System.Console.WriteLine("Возьми больше)");
+                    NumOfUser = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            System.Console.WriteLine("Красава");
+        }
+    }
+}
