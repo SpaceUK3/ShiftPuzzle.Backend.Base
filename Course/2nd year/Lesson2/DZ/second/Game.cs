e511dcd3fe7c18e192b70f08e65de9272b150739
SpaceUK <71958442+SpaceUK3@users.noreply.github.com>
2024-10-08 16:44:06 +0300

    Update Game.cs

--- Course/2nd year/Lesson2/DZ/second/Game.cs
+++ Course/2nd year/Lesson2/DZ/second/Game.cs
@@ -8,7 +8,6 @@ namespace sfg
         {
             Random rnd = new Random();
             int random = rnd.Next(1, 15);
-            System.Console.WriteLine(random);
             System.Console.WriteLine("Я загадал число от 1 до 15, попробуй его угадать :)");
             int NumOfUser = Convert.ToInt32(Console.ReadLine());
             while (NumOfUser != random) 
